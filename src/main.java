bb68f401c2f7d2ee0285960fb616fe7416b50cf0
Onur ALTINTOP <rem00te@hotmail.com>
2022-07-12 09:43:39 +0300

    Initial commit

---
+++ src/main.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args) {
+        int a, b, c;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print(" A SAYISI : ");
+        a = input.nextInt();
+        System.out.print(" B SAYISI : ");
+        b = input.nextInt();
+        System.out.print(" C SAYISI : ");
+        c = input.nextInt();
+
+        if ((a == b) && (a == c) && (b == c)) {
+            System.out.print(" SONUÇ : " + "Tüm sayılar eşit");
+        } else if (a < b && a < c) {
+            if (b < c) {
+                System.out.println(" SONUÇ : " + "a < b < c ");
+            } else {
+                System.out.println("SONUÇ : " + " a < c < b");
+            }
+        } else if ((b < a) && (b < c)) {
+            if (a < c) {
+                System.out.println(" b < a < c");
+            } else {
+                System.out.println(" b < c < a");
+            }
+        } else {
+            if (a < c) {
+                System.out.println(" c < a < b");
+            } else {
+                System.out.println(" c < b < a ");
+            }
+        }
+
+    }
+}
+
+
